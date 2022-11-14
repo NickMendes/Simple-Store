96406489d50263262ea59ec6c2f9f658dd822762
Nick <marina@sirq.com.br>
2022-11-14 15:04:30 -0300

    Update index.js

--- BackEnd/src/routes/index.js
+++ BackEnd/src/routes/index.js
@@ -1,7 +1,9 @@
 const ProductRoute = require('./productRoute');
 const UserRoute = require('./userRoute');
+const SaleRoute = require('./saleRoute');
 
 module.exports = {
   ProductRoute,
   UserRoute,
+  SaleRoute,
 };
